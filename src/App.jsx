92d198f19704207c80aa774a22b69464d3c069aa
itsteatv <rezashow60@gmail.com>
2023-03-17 01:04:14 +0330

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ function App() {
           </div>
         )}
         <>
-          {movies?.length > 0 && (
+          {favorites?.length > 0 && (
             <div className="favorites-container">
               <h1>favorites</h1>
               {favorites.map((favorite, index) => (
